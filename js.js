0b84ef825c67cfe1261de7bd67364eccdc40b7da
DerryAnggara <derianggaraputra06@gmail.com>
2024-05-31 14:32:24 +0700

    mantap

--- js.js
+++ js.js
@@ -22,3 +22,41 @@ function takeSnapshot() {
     document.body.appendChild(img);
 }
 
+document.getElementById("pilih-kelas").addEventListener("change", function() {
+    var selectedValue = this.value;
+    var kelas1 = document.getElementById("kelas-1");
+    var kelas2 = document.getElementById("kelas-2");
+    var kelas3 = document.getElementById("kelas-3");
+    var jurusan = document.getElementById("jurusan");
+
+    if (selectedValue === "X") {
+        kelas1.classList.remove("d-none");
+        jurusan.classList.remove("d-none")
+        kelas2.classList.add("d-none")
+        kelas3.classList.add("d-none")
+        // jurusan.style.display('d-none');
+    }else if (selectedValue === "XI"){
+        kelas2.classList.remove("d-none");
+        jurusan.classList.remove("d-none")
+        kelas1.classList.add("d-none")
+        kelas3.classList.add("d-none")
+        jurusan.style.display('block');
+
+
+    }else if (selectedValue === "XII"){
+        kelas3.classList.remove("d-none");
+        jurusan.classList.remove("d-none")
+        kelas1.classList.add("d-none")
+        kelas2.classList.add("d-none")
+        jurusan.style.display('block');
+
+
+    }else {
+        kelas1.classList.add("d-none");
+        kelas2.classList.add("d-none");
+        kelas3.classList.add("d-none");
+        jurusan.classList.add("d-none")
+
+    }
+});
+
